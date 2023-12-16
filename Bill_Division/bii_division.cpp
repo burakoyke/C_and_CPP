1c1cc7880f4686d38be52fabb8559b982d9c9cd2
burak <c.burakoyke@gmail.com>
2023-12-16 20:05:36 +0300

    Update folder structure

--- Bill_Division/bii_division.cpp
+++
@@ -1,7 +0,0 @@
-void bonAppetit(vector<int> bill, int k, int b) {
-
-    bill.erase(bill.begin()+k);
-    int b_actual = accumulate(bill.begin(), bill.end(), 0)/2;
-    (b - b_actual == 0) ? cout << "Bon Appetit" : cout << b - b_actual;
-
-}
